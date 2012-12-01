da7029926e3331e84ea5f7dca05ead1c8f6de480
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2012-12-01 11:54:19 +0100

    host only network for vagrant

--- crumble/main.py
+++ crumble/main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.run(host='0.0.0.0')
 Bootstrap(app)
 @app.route("/")
 def hello():
@@ -12,5 +11,4 @@ def go():
     app.run()
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(host='0.0.0.0')
