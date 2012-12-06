946b85ac4236dd050d1f1ff8d42dcdc5109bcbb4
Arjen Vrielink <arjenvrielink@gmail.com>
2012-12-06 23:47:25 +0100

    updated main.js so jslint is happy

--- lemonpie/static/js/main.js
+++ lemonpie/static/js/main.js
@@ -6,7 +6,7 @@ requirejs.config({
         'jquery' : 'lib/jquery',
         'underscore' : 'lib/underscore',
         'bootstrap': 'lib/bootstrap',
-        'openlayers': 'lib/OpenLayers',
+        'openlayers': 'lib/OpenLayers'
     },
     shim: {
         'bootstrap' : {
@@ -16,17 +16,18 @@ requirejs.config({
 });
 require(['jquery', 'underscore', 'bootstrap', 'openlayers'], function ($, _) {
 
-layers =  jQuery.parseJSON(layers)
-for (var layer in layers) {
-    console.log(layer, layers[layer]);
-}
+    var layers = jQuery.parseJSON(layers), layer = null;
 
-var map = L.map('map').setView([52.4, 5.8], 9);
+    for (layer in layers) {
+        console.log(layer, layers[layer]);
+    }
 
-L.tileLayer('http://{s}.tile.cloudmade.com/2a821ef633ec46428cbb11db251bac65/{styleId}/256/{z}/{x}/{y}.png', {
+    var map = L.map('map').setView([52.4, 5.8], 9);
+
+    L.tileLayer('http://{s}.tile.cloudmade.com/2a821ef633ec46428cbb11db251bac65/{styleId}/256/{z}/{x}/{y}.png', {
         styleId: 999,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
         maxZoom: 18
-}).addTo(map);
+    }).addTo(map);
 
 }); // end requirejs
