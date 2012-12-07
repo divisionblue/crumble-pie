9ee3efd91e58f6d942c8fb0602c6b559cc0d662d
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2012-12-07 15:12:02 +0100

    added csv.. todo: add csvreader and parser to geojson

--- lemonpie/lib/drop_util.py
+++ lemonpie/lib/drop_util.py
@@ -2,27 +2,43 @@ import webbrowser
 
 from dropbox import client, rest, session
 
-from lemonpie import config
-
-APP_KEY = config.DROP_KEY
-APP_SECRET = config.DROP_SECRET
+from lemonpie.config import *
 
 class Dropbox:
+    '''
+    Initialize a Dropbox session. With the dropbox session
+    it is possible to get data from the App specific folder.
+    '''
     def __init__(self):
-        sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
+        sess = session.DropboxSession(DROP_KEY, DROP_SECRET, ACCESS_TYPE)
         request_token = sess.obtain_request_token()
         try:
             url = sess.build_authorize_url(request_token)
             access_token = sess.obtain_access_token(request_token)
             self.client = client.DropboxClient(sess)
+            metadata = self.client.metadata('/').get('contents')
         except rest.ErrorResponse:
             url = sess.build_authorize_url(request_token)
             webbrowser.open(url)
-
+            raw_input()
+            access_token = sess.obtain_access_token(request_token)
+            self.client = client.DropboxClient(sess)
+            self.metadata = self.client.metadata('/').get('contents')
 
     def get_geojson(self):
-        metadata = self.client.metadata().get('contents')
-        for file_ in metadata:
+        import ipdb;ipdb.set_trace()
+        for file_ in self.metadata:
             if file_.get('mime_type') == 'application/javascript':
                 geojson = self.client.get_file(file_)
         return geojson.read()
+
+
+    def get_csv(self):
+        for file_ in self.metadata:
+            if file_.get('mime_type') == 'text/csv':
+                geojson = self.client.get_file(file_)
+        return geojson.read()
+
+def main():
+    drop_shiz = Dropbox()
+    geojson = drop_shiz.get_geojson()
