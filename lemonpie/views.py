1166cd63db75dec99063f8696e3c130929a1a7a1
Arjen Vrielink <arjenvrielink@gmail.com>
2012-12-07 17:10:30 +0100

    added dropbox geojson read to views

--- lemonpie/views.py
+++ lemonpie/views.py
@@ -1,9 +1,8 @@
 from flask import render_template, request, redirect, url_for, flash, json, g
-from flask.ext.uploads import UploadSet, configure_uploads, UploadNotAllowed, ARCHIVES
-
-from pprint import pprint
+from flask.ext.uploads import UploadSet, configure_uploads, ARCHIVES #, UploadNotAllowed
 
 from lemonpie import lemonpie
+from lemonpie.lib import drop_util
 #from lib import dropbox
 #from forms import
 
@@ -14,12 +13,23 @@ configure_uploads(lemonpie, uploaded_files)
 def to_index():
     return redirect(url_for('index'))
 
+def read_dropbox_json():
+    #json_data = open('lemonpie/static/js/data/stations.js','r')
+    #data = json.load(json_data)
+    #pprint(data)
+    #json_data.close()
+    #import ipdb; ipdb.set_trace()
+    db = drop_util.Dropbox()
+    geojson = db.get_geojson()
+    return geojson
+
 @lemonpie.route('/')
 def index():
     """index page"""
-    json_data=open('lemonpie/static/js/data/stations.js','r')
-    data = json.load(json_data)
-    #pprint(data)
-    json_data.close()
+    data = ''
+    try:
+        data = read_dropbox_json()
+    except:
+        flash('Reading dropbox failed')
     layers = {'stations': data}
     return render_template('index.html', layers=layers)
