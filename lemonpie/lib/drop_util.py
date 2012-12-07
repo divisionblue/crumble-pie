cb4622355d79f21bc012ee88fee3d9ff0f01a530
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2012-12-07 14:38:37 +0100

    added dropbox util

--- lemonpie/lib/drop_util.py
+++ lemonpie/lib/drop_util.py
@@ -7,21 +7,22 @@ from lemonpie import config
 APP_KEY = config.DROP_KEY
 APP_SECRET = config.DROP_SECRET
 
-# ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
-ACCESS_TYPE = 'app_folder'
+class Dropbox:
+    def __init__(self):
+        sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
+        request_token = sess.obtain_request_token()
+        try:
+            url = sess.build_authorize_url(request_token)
+            access_token = sess.obtain_access_token(request_token)
+            self.client = client.DropboxClient(sess)
+        except rest.ErrorResponse:
+            url = sess.build_authorize_url(request_token)
+            webbrowser.open(url)
 
-def main():
-    sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
-    request_token = sess.obtain_request_token()
-    try:
-        url = sess.build_authorize_url(request_token)
-        access_token = sess.obtain_access_token(request_token)
-        client = client.DropboxClient(sess)
-    except rest.ErrorResponse:
-        url = sess.build_authorize_url(request_token)
-        webbrowser.open(url)
-    metadata = client.metadata().get('contents')
-    for file_ in metadata:
-        if file_.get('mime_type') == 'application/javascript':
-            geojson = file_
-    return geojson
+
+    def get_geojson(self):
+        metadata = self.client.metadata().get('contents')
+        for file_ in metadata:
+            if file_.get('mime_type') == 'application/javascript':
+                geojson = self.client.get_file(file_)
+        return geojson.read()
